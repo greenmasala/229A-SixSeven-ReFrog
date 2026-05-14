9e3c13d65fad1a2e6c97414415c68a2c2b562dd1
greenmasala <sinchai2549@gmail.com>
2026-05-14 20:24:50 +0700

    Readjust Level 3

--- Assets/Scripts/Objects/Goal.cs
+++ Assets/Scripts/Objects/Goal.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
-    public GameObject LevelComplete;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
